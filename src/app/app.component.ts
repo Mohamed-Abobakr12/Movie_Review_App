4382596b49731127ca19892a00742a3ac15f8273
Mohamed Abobakr <m.abobakr.98.6@gmail.com>
2023-08-19 22:39:06 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+interface MovieSearchResult {
+  Search: {
+    Title: string;
+    Poster: string;
+  }[];
+}
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <input [(ngModel)]="title" placeholder="Enter movie title" />
+      <mat-form-field>
+        <input matInput [formControl]="titleControl" [matAutocomplete]="auto" placeholder="Enter movie title" />
+        <mat-autocomplete #auto="matAutocomplete">
+          <mat-option *ngFor="let suggestion of filteredSuggestions | async" [value]="suggestion.Title">
+            <img [src]="suggestion.Poster" alt="{{ suggestion.Title }} poster" />
+            {{ suggestion.Title }}
+          </mat-option>
+        </mat-autocomplete>
+      </mat-form-field>
       <button (click)="search()">Search</button>
     </div>
     <div *ngIf="movie">
@@ -17,22 +35,45 @@ import { HttpClient } from '@angular/common/http';
       </ul>
     </div>`,
 })
-
-export class AppComponent 
-{
-
-  title = '';
+export class AppComponent {
+  titleControl = new FormControl();
+  title = 'My App';
+  suggestions: any[] = [];
+  filteredSuggestions: Observable<any[]>;
   movie: any;
   reviews: string[] = [];
-  constructor(private http: HttpClient) {}
 
-  search() 
-  {
-    this.http.get(`http://www.omdbapi.com/?t=${this.title}&apikey=6984245c`).subscribe((data) => 
+  constructor(private http: HttpClient) {
+    this.titleControl.valueChanges.subscribe((value) => 
     {
+        this.http.get<MovieSearchResult>(`http://www.omdbapi.com/?s=${value}&apikey=6984245c`).subscribe((data) => 
+        {
+          
+          if (data.Search) {this.suggestions = data.Search;} 
+          else {console.error('Error: data returned from OMDb API is not in the expected format');}
+        });
+    });
+
+    this.filteredSuggestions = this.titleControl.valueChanges.pipe(
+      startWith(''),
+      
+      map((value) => this.filterSuggestions(value))
+    );
+  }
+
+  filterSuggestions(value: string): any[] {
+    const filterValue = value.toLowerCase();
+    return this.suggestions.filter((suggestion) =>
+      suggestion.Title.toLowerCase().includes(filterValue)
+    );
+  }
+
+  search() {
+    this.http.get(`http://www.omdbapi.com/?t=${this.titleControl.value}&apikey=6984245c`).subscribe((data) => {
       this.movie = data;
-      this.reviews = this.movie.Ratings.slice(0, 10).map((rating: { Value: string }) => rating.Value);
+      if (this.movie.Ratings) {
+        this.reviews = this.movie.Ratings.slice(0, 10).map((rating: { Value: string }) => rating.Value);
+      }
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,6 +18,12 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
     FormsModule
   ],
   providers: [],
