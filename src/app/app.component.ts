52287bdef6e6336e5c2a797195e617bb1f56b0ed
Mohamed Abobakr <m.abobakr.98.6@gmail.com>
2023-09-02 14:56:54 +0300

    newest_work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
 interface MovieSearchResult {
-  Search: {
-    Title: string;
-    Poster: string;
-  }[];
+  Search: {Title: string;Poster: string;}[];
   Error?: string;
 }
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit{
+@Component({selector: 'app-root',templateUrl: './app.component.html',styleUrls: ['./app.component.css']})
+
+export class AppComponent{
   movie: any;
   title = 'My App';
   reviews: string[] = [];
@@ -25,47 +19,25 @@ export class AppComponent implements OnInit{
   titleControl = new FormControl();
   filteredSuggestions: Observable<any[]>;
 
-  ngOnInit() {
-    const numStars = 10;
-    const starContainer = document.querySelector('.star-container') as HTMLElement;
-  
-    for (let i = 0; i < numStars; i++) {
-      const star = document.createElement('div');
-      star.classList.add('star');
-      star.style.left = Math.random() * 100 + 'vw';
-      star.style.top = Math.random() * 100 + '%';
-      star.style.zIndex = i.toString();
-      star.style.animationDuration = Math.random() * 5 + 5 + 's';
-      starContainer.appendChild(star);
-    }
-  }
-  
   constructor(private http: HttpClient) {
     this.titleControl.valueChanges.subscribe((value) => 
     {
       this.http.get<MovieSearchResult>(`http://www.omdbapi.com/?s=${value}&apikey=6984245c`).subscribe((data) => {if (data.Search) {this.suggestions = data.Search;} });
     });
-  
-    this.filteredSuggestions = this.titleControl.valueChanges.pipe(
-      startWith(''),
-      
-      map((value) => this.filterSuggestions(value))
-    );
+    this.filteredSuggestions = this.titleControl.valueChanges.pipe(startWith(''),map((value) => this.filterSuggestions(value)));
   }
 
-  filterSuggestions(value: string): any[] {
+  filterSuggestions(value: string): any[] 
+  {
     const filterValue = value.toLowerCase();
-    return this.suggestions.filter((suggestion) =>
-      suggestion.Title.toLowerCase().includes(filterValue)
-    );
+    return this.suggestions.filter((suggestion) =>suggestion.Title.toLowerCase().includes(filterValue));
   }
 
-  search() {
+  search() 
+  {
     this.http.get(`http://www.omdbapi.com/?t=${this.titleControl.value}&apikey=6984245c`).subscribe((data) => {
       this.movie = data;
-      if (this.movie.Ratings) {
-        this.reviews = this.movie.Ratings.slice(0, 10).map((rating: { Value: string }) => rating.Value);
-      }
+      if (this.movie.Ratings) {this.reviews = this.movie.Ratings.slice(0, 10).map((rating: { Value: string }) => rating.Value);}
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { FormlyModule } from '@ngx-formly/core';
-import { FormlyMaterialModule } from '@ngx-formly/material';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatAutocompleteModule,
-    NoopAnimationsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    FormlyModule.forRoot(),
-    FormlyMaterialModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule,HttpClientModule,FormsModule,ReactiveFormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
