1c88cc02f37522d00f0f3780b596020dd5bbbe2e
Mohamed Abobakr <m.abobakr.98.6@gmail.com>
2023-09-16 20:03:09 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component} from '@angular/core';
+import { Component,ElementRef,ViewChild,HostListener} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
-interface MovieSearchResult {
+interface MovieSearchResult
+{
   Search: {Title: string;Poster: string;}[];
   Error?: string;
 }
@@ -15,21 +16,45 @@ export class AppComponent{
   reviews: string[] = [];
   suggestions: any[] = [];  
   filteredSuggestions: any[] = [];
+  visibleSuggestions: any[] = []; 
   titleControl = new FormControl();
 
+  @ViewChild('searchBox') searchBox!: ElementRef;
+  @HostListener('document:click', ['$event'])
+  @HostListener('document:keydown.escape')onKeydownHandler() {this.filteredSuggestions = [];}
+  @ViewChild('suggestionsContainer', { read: ElementRef }) suggestionsContainer!: ElementRef;
+
+  onFocus() {if (this.suggestions.length) {setTimeout(() => {this.filteredSuggestions = this.suggestions;}, 200);}}
+  
+  onWheel(event: WheelEvent) 
+  {
+    event.preventDefault();
+    const elementHeight = 237;
+    if (event.deltaY < 0) {this.suggestionsContainer.nativeElement.scrollTop -= elementHeight;}
+    else {this.suggestionsContainer.nativeElement.scrollTop += elementHeight;    }
+  }
+
+  clickout(event: Event)
+  {
+    if (this.searchBox && this.searchBox.nativeElement.contains(event.target)) {}
+    else if (this.suggestionsContainer && !this.suggestionsContainer.nativeElement.contains(event.target)) {this.filteredSuggestions = [];}
+  }
+  
   constructor(private http: HttpClient) 
   {
     this.titleControl.valueChanges.subscribe((value) => 
     {
       this.http.get<MovieSearchResult>(`http://www.omdbapi.com/?s=${value}&apikey=6984245c`).subscribe((data) => 
-      {if (data.Search) {this.suggestions = data.Search;this.filteredSuggestions = this.filterSuggestions(value);}});
+      {
+        this.suggestions = data.Search || [];
+        this.filteredSuggestions = this.filterSuggestions(value);
+      });
     });
   }
-
-  filterSuggestions(value: string): any[] 
-  {
+  filterSuggestions(value: string): any[] {
     const filterValue = value.toLowerCase();
-    return this.suggestions.filter((suggestion) => suggestion.Title.toLowerCase().includes(filterValue));
+    let filtered = this.suggestions.filter((suggestion) => suggestion.Title.toLowerCase().includes(filterValue));
+    return  filtered.length ? filtered : [];
   }
 
   search() 
